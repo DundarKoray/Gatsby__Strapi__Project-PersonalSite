60141017a92117b3672d7891df01a5076739aea1
Koray Dundar <koraydundar_40@hotmail.com>
2020-04-13 15:11:28 +0300

    created courses and course components

---
+++ src/components/Courses/Courses.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Course from './Course'
+
+const Courses = () => {
+    return (
+        <div>
+           Hello from courses 
+        </div>
+    );
+};
+
+export default Courses;
